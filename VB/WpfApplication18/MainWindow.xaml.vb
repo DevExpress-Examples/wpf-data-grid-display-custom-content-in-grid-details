b521db8ffad4bc2f9971bfdcb62872b04e8eab99
DevExpressExampleBot <fakeEmail>
2022-12-19 15:37:25 +0400

    Source auto update [skip ci]

--- VB/WpfApplication18/MainWindow.xaml.vb
+++ VB/WpfApplication18/MainWindow.xaml.vb
@@ -1,201 +1,56 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Windows
-Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Grid
+Imports System.Collections.Generic
+Imports System.Collections.ObjectModel
+Imports System.Windows
 
 Namespace WpfApplication18
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			gridControl1.ItemsSource = Employees
-		End Sub
+    Public Partial Class MainWindow
+        Inherits Window
 
-		#Region "#GridInCode"
-		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Dim newGrid As GridControl = CreateGrid()
-			AddDetailsToGrid(newGrid)
-			Grid.SetRow(newGrid, 2)
-			mainGrid.Children.Add(newGrid)
-		End Sub
-		Private Function CreateGrid() As GridControl
-			Dim newGrid As New GridControl()
-            newGrid.AutoGenerateColumns = AutoGenerateColumnsMode.AddNew
-			newGrid.View = New TableView()
-			TryCast(newGrid.View, TableView).AutoWidth = True
-			TryCast(newGrid.View, TableView).ShowGroupPanel = False
-			newGrid.View.DetailHeaderContent = "Employees"
-			newGrid.ItemsSource = Employees
-			Return newGrid
-		End Function
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = Employees.GetEmployees()
+            AddHandler Me.grid.Loaded, Sub(d, e) TryCast(d, GridControl)?.ExpandMasterRow(0)
+        End Sub
 
-		Private Sub AddDetailsToGrid(ByVal grid As GridControl)
-			Dim dataControlDetail As New DataControlDetailDescriptor()
-			dataControlDetail.ItemsSourcePath = "Orders"
+        Public Class Employee
 
-			Dim detailGrid As New GridControl()
-			dataControlDetail.DataControl = detailGrid
-            detailGrid.AutoGenerateColumns = AutoGenerateColumnsMode.AddNew
-			detailGrid.View.DetailHeaderContent = "Orders"
-			TryCast(detailGrid.View, TableView).ShowGroupPanel = False
+            Public Property FirstName As String
 
-			Dim contentDetail As New ContentDetailDescriptor()
-            'contentDetail.ContentTemplate = CType(FindResource("EmployeeNotes"), DataTemplate)
+            Public Property LastName As String
 
-            Dim dt As New DataTemplate()
+            Public Property Title As String
 
-            Dim txFactory As New FrameworkElementFactory(GetType(TextBox))
-            txFactory.SetValue(TextBox.TextWrappingProperty, TextWrapping.Wrap)
-            txFactory.SetBinding(TextBox.TextProperty, New Binding("Notes"))
+            Public Property Notes As String
 
-            dt.VisualTree = txFactory
+            Public Property Orders As List(Of Order)
+        End Class
 
-            contentDetail.ContentTemplate = dt
+        Public Class Order
 
-			contentDetail.HeaderContent = "Notes"
+            Public Property Supplier As String
 
-            'Dim tabDetail As New TabViewDetailDescriptor()
-            'tabDetail.DetailDescriptors.Add(dataControlDetail)
-            'tabDetail.DetailDescriptors.Add(contentDetail)
+            Public Property [Date] As Date
 
-            grid.DetailDescriptor = contentDetail
-		End Sub
-		#End Region ' #GridInCode
+            Public Property ProductName As String
 
-		Private employees_Renamed As List(Of Employee)
-		Private ReadOnly Property Employees() As List(Of Employee)
-			Get
-				If employees_Renamed Is Nothing Then
-					PopulateEmployees()
-				End If
-				Return employees_Renamed
+            Public Property Quantity As Integer
+        End Class
 
-			End Get
-		End Property
-		Private Sub PopulateEmployees()
-			employees_Renamed = New List(Of Employee)()
-			employees_Renamed.Add(New Employee("Bruce", "Cambell", "Sales Manager", "Education includes a BA in psychology from Colorado State University in 1970.  He also completed 'The Art of the Cold Call.'  Bruce is a member of Toastmasters International."))
-			employees_Renamed(0).Orders.Add(New Order("Suppliers Inc.", DateTime.Now, "TV", 20))
-			employees_Renamed(0).Orders.Add(New Order("Suppliers Inc.", DateTime.Now.AddDays(3), "Projector", 15))
-			employees_Renamed.Add(New Employee("Cindy", "Haneline", "Vice President of Sales", "Cindy received her BTS commercial in 1974 and a Ph.D. in international marketing from the University of Dallas in 1981.  She is fluent in French and Italian and reads German.  She joined the company as a sales representative, was promoted to sales manager in January 1992 and to vice president of sales in March 1993.  Cindy is a member of the Sales Management Roundtable, the Seattle Chamber of Commerce, and the Pacific Rim Importers Association."))
-			employees_Renamed(1).Orders.Add(New Order("Suppliers Inc.", DateTime.Now.AddDays(1), "Blu-Ray Player", 10))
-		End Sub
+        Public Class Employees
 
-	End Class
-
-	Public Class Employee
-		Private firstName_Renamed As String
-		Private lastName_Renamed As String
-		Private title_Renamed As String
-		Private notes_Renamed As String
-		Private orders_Renamed As List(Of Order)
-		Public Sub New(ByVal firstName As String, ByVal lastName As String, ByVal title As String, ByVal notes As String)
-			Me.firstName_Renamed = firstName
-			Me.lastName_Renamed = lastName
-			Me.title_Renamed = title
-			Me.notes_Renamed = notes
-			Me.orders_Renamed = New List(Of Order)()
-		End Sub
-		Public Sub New()
-		End Sub
-		Public Property FirstName() As String
-			Get
-				Return firstName_Renamed
-			End Get
-			Set(ByVal value As String)
-				firstName_Renamed = value
-			End Set
-		End Property
-		Public Property LastName() As String
-			Get
-				Return lastName_Renamed
-			End Get
-			Set(ByVal value As String)
-				lastName_Renamed = value
-			End Set
-		End Property
-		Public Property Title() As String
-			Get
-				Return title_Renamed
-			End Get
-			Set(ByVal value As String)
-				title_Renamed = value
-			End Set
-		End Property
-		Public Property Notes() As String
-			Get
-				Return notes_Renamed
-			End Get
-			Set(ByVal value As String)
-				notes_Renamed = value
-			End Set
-		End Property
-		Public ReadOnly Property Orders() As List(Of Order)
-			Get
-				Return orders_Renamed
-			End Get
-		End Property
-	End Class
-
-	Public Class Order
-		Private supplier_Renamed As String
-		Private date_Renamed As DateTime
-		Private productName_Renamed As String
-		Private quantity_Renamed As Integer
-		Public Sub New(ByVal supplier As String, ByVal [date] As DateTime, ByVal productName As String, ByVal quantity As Integer)
-			Me.supplier_Renamed = supplier
-			Me.date_Renamed = [date]
-			Me.productName_Renamed = productName
-			Me.quantity_Renamed = quantity
-		End Sub
-		Public Property Supplier() As String
-			Get
-				Return supplier_Renamed
-			End Get
-			Set(ByVal value As String)
-				supplier_Renamed = value
-			End Set
-		End Property
-		Public Property [Date]() As DateTime
-			Get
-				Return date_Renamed
-			End Get
-			Set(ByVal value As DateTime)
-				date_Renamed = value
-			End Set
-		End Property
-		Public Property ProductName() As String
-			Get
-				Return productName_Renamed
-			End Get
-			Set(ByVal value As String)
-				productName_Renamed = value
-			End Set
-		End Property
-		Public Property Quantity() As Integer
-			Get
-				Return quantity_Renamed
-			End Get
-			Set(ByVal value As Integer)
-				quantity_Renamed = value
-			End Set
-		End Property
-	End Class
+            Public Shared Function GetEmployees() As ObservableCollection(Of Employee)
+                Dim employees As ObservableCollection(Of Employee) = New ObservableCollection(Of Employee)() From {New Employee() With {.FirstName = "Bruce", .LastName = "Cambell", .Title = "Sales Manager", .Notes = "Education includes a BA in psychology from Colorado State University in 1970.  He also completed 'The Art of the Cold Call.'  Bruce is a member of Toastmasters International.", .Orders = New List(Of Order)()}, New Employee() With {.FirstName = "Cindy", .LastName = "Haneline", .Title = "Vice President of Sales", .Notes = "Cindy received her BTS commercial in 1974 and a Ph.D. in international marketing from the University of Dallas in 1981.  She is fluent in French and Italian and reads German.  She joined the company as a sales representative, was promoted to sales manager in January 1992 and to vice president of sales in March 1993.  Cindy is a member of the Sales Management Roundtable, the Seattle Chamber of Commerce, and the Pacific Rim Importers Association.", .Orders = New List(Of Order)()}}
+                employees(0).Orders.Add(New Order() With {.Supplier = "Supplier 1", .[Date] = Date.Now, .ProductName = "TV", .Quantity = 20})
+                employees(0).Orders.Add(New Order() With {.Supplier = "Supplier 2", .[Date] = Date.Now.AddDays(3), .ProductName = "Projector", .Quantity = 15})
+                employees(0).Orders.Add(New Order() With {.Supplier = "Supplier 3", .[Date] = Date.Now.AddDays(3), .ProductName = "HDMI Cable", .Quantity = 50})
+                employees(1).Orders.Add(New Order() With {.Supplier = "Supplier 1", .[Date] = Date.Now.AddDays(1), .ProductName = "Blu-Ray Player", .Quantity = 10})
+                employees(1).Orders.Add(New Order() With {.Supplier = "Supplier 2", .[Date] = Date.Now.AddDays(1), .ProductName = "HDMI Cable", .Quantity = 10})
+                employees(1).Orders.Add(New Order() With {.Supplier = "Supplier 3", .[Date] = Date.Now.AddDays(1), .ProductName = "Projector", .Quantity = 10})
+                employees(1).Orders.Add(New Order() With {.Supplier = "Supplier 4", .[Date] = Date.Now.AddDays(1), .ProductName = "Amplifier", .Quantity = 10})
+                Return employees
+            End Function
+        End Class
+    End Class
 End Namespace
